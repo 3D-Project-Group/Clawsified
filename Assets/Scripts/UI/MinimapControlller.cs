1870093f75c929a8270dd79730d84f3a3fee8f16
Khevynn Sá <115984821+Khevynn@users.noreply.github.com>
2024-04-29 16:20:28 +0100

    Minimap

---
+++ Assets/Scripts/UI/MinimapControlller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapControlller : MonoBehaviour
+{
+    private Transform player;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 newPosition = player.position;
+        newPosition.y = transform.position.y;
+        transform.position = newPosition;
+    }
+}
